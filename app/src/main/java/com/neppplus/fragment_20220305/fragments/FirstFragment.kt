342afaa74078121374a840f1678bd4edacd956c5
skyoo1226 <you1226@naver.com>
2022-03-05 12:49:31 +0900

    첫번째 프래그먼트 생성 - 프래그먼트로 상속 받게 해줌 androidx가 주는 클래스 선택 후 onCreateView와 return inflater.inflate(... 사용 -동작 관련 함수 수정

--- app/src/main/java/com/neppplus/fragment_20220305/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20220305/fragments/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.fragment_20220305.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20220305.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -16,6 +18,15 @@ class FirstFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
 
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLoginFragment.setOnClickListener {
+            Log.d("프래그먼트", "첫번째 로그 찍기")
+
+        }
     }
 
 }
